dacb824d1071bd96b773631ef35c48a3dcf8577b
Howard Tai <howard.tai.618@gmail.com>
2023-08-04 17:24:14 +0800

    Create scripts for setup.

---
+++ scripts/build_and_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Build the docker images and use docker-compose to start the containers
+docker-compose up --build -d
---
+++ scripts/trigger_etl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Trigger the ETL process by making a POST request
+curl -X POST http://localhost:80/trigger_etl
